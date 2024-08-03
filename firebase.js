7218cb27771a91ac71f86db2b63147687b6c3fc1
yejulee <yl1657@scarletmail.rutgers.edu>
2024-08-03 17:38:29 -0400

    base outline created

--- firebase.js
+++ firebase.js
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFireStore } from "firebase/firestore";
+import { getFirestore, getFireStore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,7 +18,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const firestore = getFireStore(app);
+const firestore = getFirestore(app);
 
 export {firestore}
